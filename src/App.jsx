a59d7c0459f6a3c72194b756577696bdb1a74cbd
John Paul <johnpaul@trees.local>
2022-07-12 11:01:07 +0300

    add skeleton for components

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Services from "./components/services/Services";
+import Nav from "./components/nav/Nav";
+
+const App = () => {
+  return (
+    <div>
+      <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </>
+    </div>
+  );
+};
+
+export default App;
