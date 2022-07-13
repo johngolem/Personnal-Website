c41a2f93ad9fa23df42fccdbbc77d17867b37273
John Paul <johnpaul@trees.local>
2022-07-13 12:02:57 +0300

    start styling header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { ImGithub } from "react-icons/im";
+import { FaTwitter } from "react-icons/fa";
 
-const headerSocials = () => {
+const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank"></a>
-      <a href="https://github.com" target="_blank"></a>
-      <a href="https://dribble.com" target="_blank"></a>
-      headerSocials
+      <a href="https://linkedin.com" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank">
+        <ImGithub />
+      </a>
+      <a href="https://dribble.com" target="_blank">
+        <FaTwitter />
+      </a>
     </div>
   );
 };
 
-export default headerSocials;
+export default HeaderSocials;
