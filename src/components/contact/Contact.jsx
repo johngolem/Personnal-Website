17f5e69f61fb6d5ed9d21de966a595d66ee560a6
John Paul <johnpaul@trees.local>
2022-07-14 17:11:18 +0300

    create sections:

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-  return <section id="section">Contact</section>;
+  return <section id="contact">Contact</section>;
 };
 
 export default Contact;
