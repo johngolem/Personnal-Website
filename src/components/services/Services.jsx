629da2ef1eae076e818eafaad867a2bbdf605fe0
John Paul <johnpaul@trees.local>
2022-07-28 19:32:38 +0300

    creating my services container

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,101 @@
 import React from "react";
 import "./services.css";
-
+import { BsPatchCheck } from "react-icons/bs";
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>My Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* start of web development  */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* start of mobile development */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Mobile Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsPatchCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
