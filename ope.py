14cbea9b489fd61a12b4a76b36b89c51fbb34297
John Paul <johnpaul@trees.local>
2022-07-28 12:39:14 +0300

    start working on experience section, the three stack container

--- ope.py
+++ ope.py
@@ -1,9 +1,13 @@
-print('What is your name')
-answer = input()
-print('Hello, ' + answer + '!')
+# num1 = int(input('enter a number: '))
+# num2= int(input('enter a second number: '))
+# num3=int(input('enter a third number: '))
+
+# num4= num1*num2/num3
+
+# print('num4 is equal to: ', num4)
 
 
+dic=[]
 
-
-
-
+dic={1:"geeks", 2:"for", 3:"geeks"}
+print(dic)
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,99 @@
 import React from "react";
 import "./experience.css";
+import { BsStack } from "react-icons/bs";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>My Skills</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsStack />
+              <h4>Flutter</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>HTML</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>Bootstrap</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>CSS</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>REACT</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>ANGULAR</h4>
+              <small>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsStack />
+              <h4>Django</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>Nodejs</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>Laravel</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>Docker</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>Kubernetes</h4>
+              <small>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsStack />
+              <h4>Postgres SQL</h4>
+              <small>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__mobile">
+          <h3>Mobile Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsStack />
+              <h4>Django</h4>
+              <small>Experienced</small>
+            </article>
+            <div />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
