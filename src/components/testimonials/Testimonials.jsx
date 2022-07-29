d37626eb38f1d3f0770ef7071f903eced4817c77
John Paul <johnpaul@trees.local>
2022-07-29 11:17:24 +0300

    made portfolio data dynamically typed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./testimonials.css";
 
+import { useState } from "react";
+
+function Example() {
+  const [deduct, setDeduct] = useState(45);
+  return (
+    <div>
+      <p>You clicked deduct {deduct} times</p>
+      <button onClick={() => setDeduct(deduct - 1)}>Deduct</button>
+    </div>
+  );
+}
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return <section id="testimonials">Example()</section>;
 };
 
 export default Testimonials;
