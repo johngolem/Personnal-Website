54faf67b0efffcfdf6f5a393bfcaf61f41e2dfa0
John Paul <johnpaul@trees.local>
2022-07-29 11:52:52 +0300

    made review data dynamic

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import "./testimonials.css";
 import avt1 from "../../assets/avatar1.jpeg";
 
-import { useState } from "react";
+const data = [
+  {
+    avatar: avt1,
+    name: "Nottech Lab",
+    review:
+      "loren ipsum dolor sit amet consectetur adipisicing elit. Laboriosam velit dolores perferendis minima ab laudantium hic sed! Corrupti itaque dignissimos ea beatae similique, numquam nostrum suscipit ducimus unde temporibus amet!",
+  },
+  {
+    avatar: avt1,
+    name: "Nottech Lab",
+    review:
+      "loren ipsum dolor sit amet consectetur adipisicing elit. Laboriosam velit dolores perferendis minima ab laudantium hic sed! Corrupti itaque dignissimos ea beatae similique, numquam nostrum suscipit ducimus unde temporibus amet!",
+  },
+  {
+    avatar: avt1,
+    name: "Nottech Lab",
+    review:
+      "loren ipsum dolor sit amet consectetur adipisicing elit. Laboriosam velit dolores perferendis minima ab laudantium hic sed! Corrupti itaque dignissimos ea beatae similique, numquam nostrum suscipit ducimus unde temporibus amet!",
+  },
+];
 
-// function Example() {
-//   const [deduct, setDeduct] = useState(45);
-//   return (
-//     <div>
-//       <p>You clicked deduct {deduct} times</p>
-//       <button onClick={() => setDeduct(deduct - 1)}>Deduct</button>
-//     </div>
-//   );
-// }
 const Testimonials = () => {
   return (
     <section id="testimonials">
@@ -20,42 +30,17 @@ const Testimonials = () => {
       <h2>What people say</h2>
 
       <div className=" container testimonials-container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avt1} alt="avatar1" />
-          </div>
-          <h5 className="client__name">Nottech Lab</h5>
-          <small className="client__review">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-            velit dolores perferendis minima ab laudantium hic sed! Corrupti
-            itaque dignissimos ea beatae similique, numquam nostrum suscipit
-            ducimus unde temporibus amet!
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avt1} alt="avatar1" />
-          </div>
-          <h5 className="client__name">Nottech Lab</h5>
-          <small className="client__review">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-            velit dolores perferendis minima ab laudantium hic sed! Corrupti
-            itaque dignissimos ea beatae similique, numquam nostrum suscipit
-            ducimus unde temporibus amet!
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avt1} alt="avatar1" />
-          </div>
-          <h5 className="client__name">Nottech Lab</h5>
-          <small className="client__review">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
-            velit dolores perferendis minima ab laudantium hic sed! Corrupti
-            itaque dignissimos ea beatae similique, numquam nostrum suscipit
-            ducimus unde temporibus amet!
-          </small>
-        </article>
+        {data.map(({ avatar, name, review }) => {
+          return (
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="avatar1" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
